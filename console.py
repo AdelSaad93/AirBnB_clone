f5dc05e4b1557d76d19e2a3d9d88cb9f80d39ec6
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-05-19 22:39:47 -0700

    var

--- console.py
+++ console.py
@@ -116,5 +116,22 @@ class HBNBCommand(cmd.Cmd):
         obj.save()
 
 
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ tests/test_console.py
@@ -0,0 +1,36 @@
+import unittest
+from io import StringIO
+from console import HBNBCommand
+import sys
+
+
+class TestConsole(unittest.TestCase):
+    def setUp(self):
+        """Redirect stdout to capture print statements"""
+        self.old_stdout = sys.stdout
+        sys.stdout = self.mystdout = StringIO()
+
+    def tearDown(self):
+        """Reset stdout"""
+        sys.stdout = self.old_stdout
+
+    def test_quit(self):
+        """Test the quit command"""
+        self.assertEqual(HBNBCommand().onecmd("quit"), True)
+
+    def test_EOF(self):
+        """Test the EOF command"""
+        self.assertEqual(HBNBCommand().onecmd("EOF"), True)
+
+    def test_empty_line(self):
+        """Test empty line"""
+        HBNBCommand().onecmd("")
+        self.assertEqual(self.mystdout.getvalue(), "")
+
+    def test_empty_line_with_spaces(self):
+        """Test empty line with spaces"""
+        HBNBCommand().onecmd("    ")
+        self.assertEqual(self.mystdout.getvalue(), "")
+
+if __name__ == '__main__':
+    unittest.main()
